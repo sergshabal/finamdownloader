f1297bfef7c82adfe6565470171796e3dc476ea6
serg <sergshabal@gmail.com>
2013-10-15 11:34:26 +0600

    gfgfg

--- finamdownloader/finamquote_dwl.py
+++ finamdownloader/finamquote_dwl.py
@@ -4,33 +4,35 @@ from urllib2 import urlopen, Request
 from datetime import datetime, timedelta, date
 
 finam_symbols = urlopen('http://www.finam.ru/cache/icharts/icharts.js').readlines()
-periods = { 'tick' : 1, 'min' : 2, '5min' : 3, '10min' : 4, '15min' : 5,
-            '30min' : 6, 'hour' : 7, 'daily' : 8, 'week' : 9, 'month' : 10 }
+periods = {'tick': 1, 'min': 2, '5min': 3, '10min': 4, '15min': 5,
+           '30min': 6, 'hour': 7, 'daily': 8, 'week': 9, 'month': 10}
 
 __all__ = ['periods', 'get_quotes_finam']
-			
+
+
 def __get_finam_code__(symbol):
     s_id = finam_symbols[0]
     s_code = finam_symbols[2]
-    names = s_code[s_code.find('[\'')+1:s_code.find('\']')].split('\',\'')
-    ids = s_id[s_id.find('[')+1:s_id.find(']')].split(',')
-    if (symbol in names):
+    names = s_code[s_code.find('[\'') + 1:s_code.find('\']')].split('\',\'')
+    ids = s_id[s_id.find('[') + 1:s_id.find(']')].split(',')
+    if symbol in names:
         symb_id = names.index(symbol)
         return int(ids[symb_id])
     else:
         print "%s not found\r\n" % symbol
     return 0
 
+
 def __get_url__(symbol, period, start_date, end_date):
     finam_HOST = "195.128.78.52"
     #finam_URL = "/table.csv?d=d&market=1&f=table&e=.csv&dtf=1&tmf=1&MSOR=0&sep=1&sep2=1&at=1&"
     finam_URL = "/table.csv?d=d&market=1&f=table&e=.csv&dtf=1&tmf=3&MSOR=0&mstime=on&mstimever=1&sep=3&sep2=1&at=1&"
-               #'/table.csv?d=d&market=1&f=table&e=.csv&dtf=1&tmf=3&MSOR=0&mstime=on&mstimever=1&sep=3&sep2=1&at=1'
+    #'/table.csv?d=d&market=1&f=table&e=.csv&dtf=1&tmf=3&MSOR=0&mstime=on&mstimever=1&sep=3&sep2=1&at=1'
     symb = __get_finam_code__(symbol)
     params = urlencode({"p": period, "em": symb,
-                        "df": start_date.day, "mf": start_date.month-1,
+                        "df": start_date.day, "mf": start_date.month - 1,
                         "yf": start_date.year,
-                        "dt": end_date.day, "mt": end_date.month-1,
+                        "dt": end_date.day, "mt": end_date.month - 1,
                         "yt": end_date.year, "cn": symbol})
 
     stock_URL = finam_URL + params
@@ -39,20 +41,23 @@ def __get_url__(symbol, period, start_date, end_date):
     else:
         return "http://" + finam_HOST + stock_URL + '&datf=5'
 
+
 def __period__(s):
     return periods[s]
 
+
 def __get_daily_quotes_finam__(symbol, start_date='20070101', end_date=date.today().strftime('%Y%m%d'),
-                           period='daily'):
+                               period='daily'):
     """
     Return downloaded daily or more prices about symbol from start date to end date
     """
     start_date = datetime.strptime(start_date, "%Y%m%d").date()
     end_date = datetime.strptime(end_date, "%Y%m%d").date()
     url = __get_url__(symbol, __period__(period), start_date, end_date)
-    pdata = read_csv(url, index_col=0, parse_dates={'index':[0,1]}, sep=';').sort_index()
-    pdata.columns = [ symbol + '.' + i for i in ['Open', 'High', 'Low', 'Close', 'Vol'] ]
+    pdata = read_csv(url, index_col=0, parse_dates={'index': [0, 1]}, sep=';').sort_index()
+    pdata.columns = [symbol + '.' + i for i in ['Open', 'High', 'Low', 'Close', 'Vol']]
     return pdata
+
 
 def get_quotes_finam(symbol, start_date='20070101', end_date=date.today().strftime("%Y%m%d"),
                      period='daily'):
@@ -69,29 +74,31 @@ def get_quotes_finam(symbol, start_date='20070101', end_date=date.today().strfti
         start_date = datetime.strptime(start_date, "%Y%m%d").date()
         end_date = datetime.strptime(end_date, "%Y%m%d").date()
         url = __get_url__(symbol, __period__(period), start_date, end_date)
-        pdata = read_csv(url, index_col=0, parse_dates={'index':[0,1]}, sep=';').sort_index()
-        pdata.columns = [ symbol + '.' + i for i in ['Open', 'High', 'Low', 'Close', 'Vol'] ]
+        pdata = read_csv(url, index_col=0, parse_dates={'index': [0, 1]}, sep=';').sort_index()
+        pdata.columns = [symbol + '.' + i for i in ['Open', 'High', 'Low', 'Close', 'Vol']]
         return pdata
+
 
 def __get_tick_quotes_finam__(symbol, start_date, end_date):
     start_date = datetime.strptime(start_date, "%Y%m%d").date()
     end_date = datetime.strptime(end_date, "%Y%m%d").date()
     delta = end_date - start_date
     data = DataFrame()
-    for i in range(delta.days+1):
+    for i in range(delta.days + 1):
         day = timedelta(i)
         url = __get_url__(symbol, periods['tick'], start_date + day, start_date + day)
         req = Request(url)
         req.add_header('Referer', 'http://www.finam.ru/analysis/profile0000300007/default.asp')
         r = urlopen(req)
 
-        tmp_data = read_csv(r, index_col=0, parse_dates={'index':[0,1]}, sep=';').sort_index()
+        tmp_data = read_csv(r, index_col=0, parse_dates={'index': [0, 1]}, sep=';').sort_index()
         if data.empty:
             data = tmp_data
         else:
             data = data.append(tmp_data)
-    data.columns = [ symbol + '.' + i for i in ['Last', 'Vol', 'Id'] ]
+    data.columns = [symbol + '.' + i for i in ['Last', 'Vol', 'Id']]
     return data
+
 
 def __get_tick_quotes_finam_all__(symbol, start_date, end_date):
     start_date = datetime.strptime(start_date, "%Y%m%d").date()
@@ -100,13 +107,14 @@ def __get_tick_quotes_finam_all__(symbol, start_date, end_date):
     req = Request(url)
     req.add_header('Referer', 'http://www.finam.ru/analysis/profile0000300007/default.asp')
     r = urlopen(req)
-    pdata = read_csv(r, index_col=0, parse_dates={'index':[0,1]}, sep=';').sort_index()
-    pdata.columns = [ symbol + '.' + i for i in ['Last', 'Vol', 'Id'] ]
+    pdata = read_csv(r, index_col=0, parse_dates={'index': [0, 1]}, sep=';').sort_index()
+    pdata.columns = [symbol + '.' + i for i in ['Last', 'Vol', 'Id']]
     return pdata
+
 
 if __name__ == "__main__":
     code = 'FEES'
     per = 'daily'
-    print 'download %s data for %s' %(per, code)
+    print 'download %s data for %s' % (per, code)
     quote = get_quotes_finam(code, start_date='20130513', period=per)
     print quote.head()
